52ca7f89e11b2e6d200500ddaf5858887844eb38
zxt000816 <zyf13815628123@gmail.com>
2023-10-12 14:44:08 +0900

    modified:   eval.py 	modified:   transformer/eval.py

--- eval.py
+++ eval.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
-import torch
-from torch.utils.data import DataLoader
-from torch import nn
-from datetime import datetime
+import os
+# from torch.utils.data import DataLoader
+# from torch import nn
+# from datetime import datetime
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
 from typing import List, Dict, Tuple, Union, Any, Optional
 import matplotlib.pyplot as plt
-from timeseries.dataset import InformerDataset
+# from timeseries.dataset import InformerDataset
+import matplotlib.dates as mdates
 
 def save_prediction_performance(
         prediction_result_eval_path: str, 
@@ -45,33 +46,88 @@ def error_report(ground_truth: Union[List, np.ndarray], predictions: Union[List,
     r2 = r2_score(ground_truth, predictions)
 
     return {
-        'RMSE': rmse,
-        'MAPE': mape,
-        'R2': r2,
+        'RMSE': float(rmse),
+        'MAPE': float(mape),
+        'R2': float(r2),
     }
 
-def save_plot_img(
-    img_save_path: str, 
-    pred: Union[List, np.ndarray], 
-    real: Union[List, np.ndarray], 
-    mape: float, 
-    rmse: float, 
-    shreshold: float,
-    x_axis: Union[List, np.ndarray] = [], 
-    show: bool = False
-) -> None:
-    plt.figure(figsize=(18, 6))
-    if len(x_axis) > 0: 
-        plt.plot(x_axis, real, label="real")
-        plt.plot(x_axis, pred, label="predict")
-    else:
-        plt.plot(real, label="real")
-        plt.plot(pred, label="predict")
+def timeseries_plot(
+    ground_truth: Union[np.ndarray, List],
+    predictions: Union[np.ndarray, List],
+    x_axis: pd.Series,
+    freq: str,
+    figsize: Tuple=(10, 6),
+    angle: int=45,
+    interval: Optional[int]=None,
+    axvline: Optional[pd.Timestamp]=None,
+    save_path: Optional[str]=None,
+    show: bool=True,
+):
+    if interval is None:
+        interval= int(ground_truth.shape[0] / 20)
     
-    plt.xticks(rotation=90)
+    x_axis_locator, x_axis_offset = None, None
+    if freq == 'M':
+        x_axis_format = '%Y-%m'
+        x_axis_locator = mdates.MonthLocator(interval=interval)
+        x_axis_offset = pd.DateOffset(months=1)
+    else:
+        x_axis_format = '%Y-%m-%d'
+        x_axis_locator = mdates.DayLocator(interval=interval)
+        x_axis_offset = pd.DateOffset(days=1)
+
+    plt.figure(figsize=figsize)
+    plt.plot(x_axis, ground_truth, label="ground-truth", marker='o')
+    plt.plot(x_axis, predictions, label="predictions", marker='o')
+    plt.xticks(rotation=angle)
     plt.legend()
-    plt.title(f'MAPE: {mape*100:.2f}%  RMSE / Shreshold: {rmse:.2f} / {shreshold:.2f}')
-    plt.savefig(img_save_path)
-    if show:
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter(x_axis_format))
+    plt.gca().xaxis.set_major_locator(x_axis_locator)
+    plt.gca().xaxis.set_tick_params(rotation=angle)
+    plt.gca().set_xbound(
+        x_axis.min()-x_axis_offset, 
+        x_axis.max()+x_axis_offset
+    )
+    plt.grid()
+    if axvline is not None:
+        plt.axvline(x=axvline, color='r', linestyle='--', label='train-test split')
+
+    if save_path is not None:
+        if not os.path.exists(save_path):
+            plt.savefig(save_path)
+        else:
+            print(f'{save_path} already exists')
+    
+    if show is True:
         plt.show()
+
     plt.close()
+
+    
+
+# def save_plot_img(
+#     img_save_path: str, 
+#     pred: Union[List, np.ndarray], 
+#     real: Union[List, np.ndarray], 
+#     mape: float, 
+#     rmse: float, 
+#     shreshold: float,
+#     x_axis: Union[List, np.ndarray] = [], 
+#     show: bool = False
+# ) -> None:
+#     plt.figure(figsize=(18, 6))
+#     if len(x_axis) > 0: 
+#         plt.plot(x_axis, real, label="real")
+#         plt.plot(x_axis, pred, label="predict")
+#     else:
+#         plt.plot(real, label="real")
+#         plt.plot(pred, label="predict")
+    
+#     plt.xticks(rotation=90)
+#     plt.legend()
+#     plt.title(f'MAPE: {mape*100:.2f}%  RMSE / Shreshold: {rmse:.2f} / {shreshold:.2f}')
+#     plt.savefig(img_save_path)
+#     if show:
+#         plt.show()
+#     plt.close()
--- transformer/eval.py
+++ transformer/eval.py
@@ -10,16 +10,16 @@ def parse_informerPL_output(outputs, pred_len):
 
     _preds = _preds.cpu().numpy()
     _trues = _trues.cpu().numpy()
-
-    # if scaler is not None:
-    #     _preds = scaler.inverse_transform(_preds)
-    #     _trues = scaler.inverse_transform(_trues)
+    
+    num_of_samples = len(_preds)
+    if num_of_samples % pred_len == 0:
+        point = num_of_samples - pred_len
+    else:
+        point = num_of_samples - (num_of_samples % pred_len)
 
     all_preds = []
     all_trues = []
-    num_of_samples = len(_preds)
     for i in range(num_of_samples):
-        point = num_of_samples - (num_of_samples % pred_len)
         if i < point:
             if (i % pred_len == 0):
                 all_preds.append(_preds[i])
